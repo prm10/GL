3509da65be68721ff1e44171240c97ecd543314e
prm14 <prm@datrain.cn>
2015-11-06 16:54:56 +0800

    a

--- gradient_check.m
+++ gradient_check.m
@@ -5,9 +5,9 @@ args.label=[mean(args.input,2)<=0.5 mean(args.input,2)>0.5];
 args.numblocks=5;
 args=LSTM_initial(args);
 [~,~,dw_i]=LSTM_ff(args);
-args.Weight.w_i(1,1)=args.Weight.w_i(1,1)+1e-8;
+args.Weight.w_i(1,1)=args.Weight.w_i(1,1)+1e-10;
 [err1]=LSTM_ff(args);
-args.Weight.w_i(1,1)=args.Weight.w_i(1,1)-2e-8;
+args.Weight.w_i(1,1)=args.Weight.w_i(1,1)-2e-10;
 [err2]=LSTM_ff(args);
-(err1-err2)/2e-8
+(err1-err2)/2e-10
 dw_i(1,1)
