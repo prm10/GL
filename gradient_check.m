4c1169c951f92b6dca6f0ea5ac3964f263a2a113
prm10 <353345737@qq.com>
2015-11-13 23:25:09 +0800

    gradient check finish

--- gradient_check.m
+++ gradient_check.m
@@ -1,10 +1,10 @@
 clc;close all;clear all;
 rand('seed',2);
-args.input=rand(5,3);
+args.input=rand(15,3);
 args.label=[mean(args.input,2)<=0.5 mean(args.input,2)>0.5];
 args.numblocks=5;
 args=LSTM_initial(args);
-delta=1e-8;
+delta=1e-10;
 
 % post=10;
 % pos=2;
@@ -14,16 +14,16 @@ delta=1e-8;
 % d2=(err1-err2)/2/delta
 % d1(post,pos)
 
-for post=1:5
-    for pos=1:5
-        [~,d1]=LSTM_ff(args,0,post,pos);
-        % args.Weight.w_i(1,1)=args.Weight.w_i(1,1)+delta;
-        [err1,~]=LSTM_ff(args,delta,post,pos);
-        % args.Weight.w_i(1,1)=args.Weight.w_i(1,1)-2*delta;
-        [err2,~]=LSTM_ff(args,-delta,post,pos);
-        d2(post,pos)=(err1-err2)/2/delta;
-    end
-end
+% for post=1:15
+%     for pos=1:3
+%         [~,d1]=LSTM_ff(args,0,post,pos);
+%         % args.Weight.w_i(1,1)=args.Weight.w_i(1,1)+delta;
+%         [err1,~]=LSTM_ff(args,delta,post,pos);
+%         % args.Weight.w_i(1,1)=args.Weight.w_i(1,1)-2*delta;
+%         [err2,~]=LSTM_ff(args,-delta,post,pos);
+%         d2(post,pos)=(err1-err2)/2/delta;
+%     end
+% end
 
 
 
