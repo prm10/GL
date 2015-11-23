b5c8fbeb624de37bc1f0fa701a45cc03c5a0015e
prm14 <prm@datrain.cn>
2015-11-23 19:02:25 +0800

    come into a cheos again

--- GL_lstm_unsupervised.m
+++ GL_lstm_unsupervised.m
@@ -7,6 +7,16 @@ load('K:\GL_data\3\data_normalized.mat');
 % plot(date0(range),data0(range,6));
 % datestr(date0(1920468),'yyyy-mm-dd HH:MM:SS')
 %% 3号高炉，2012-07-06至年底为训练集；2013年为测试集
+global train_data train_label test_data test_label;
 train_data=data1(400000:1920468,:);
 test_data=data1(1920468:end,:);
 clear data1;
+args.layer=[size(train_input{1},2) 20 5 size(train_label{1},2)];
+args.maxecho=100;
+args.momentum=0.9;
+args.labellength=8000;
+args.circletimes=100;
+args.learningrate=1e-3;
+args.outputtype='softmax';
+args=LSTM_initial(args);
+[args]=LSTM_train(args,train_input,train_label);
--- LSTM_train.m
+++ LSTM_train.m
@@ -1,10 +1,16 @@
-function [args]=LSTM_train(args,input,label)
+function [args]=LSTM_train(args)
+    global train_data train_label test_data test_label;
+    n=size(train_data)-args.labellength;
     for i1=1:args.maxecho
-        for i2=1:length(input)%对单个样本梯度下降
-            [args]=LSTM_ff_bp(args,input{i2},label{i2});
+        index=floor(rand(args.circletimes,1)*n);
+        for i2=1:args.circletimes%对单个样本梯度下降
+            range=index(i2)+1:index(i2)+args.labellength;
+            input=train_data(range,:);
+            label=train_label(range,:);
+            [args]=LSTM_ff_bp(args,input,label);
         end
        %% 统计误差
-        [~,error]=LSTM_ff(input,label,args);
+        [~,error]=LSTM_ff(test_data,test_label,args);
         fprintf('%d train error: %.4f \n',i1,error);
     end
 
