6c1b19d255711d0fa89966279436c533c82d10d5
prm14 <prm@datrain.cn>
2015-12-04 10:06:29 +0800

    a

--- translation/Gradient_check.m
+++ translation/Gradient_check.m
@@ -3,14 +3,14 @@ rng(3);
 T=500;
 t=[1:T]';
 x1=(sin(t/pi)+sin(t*0.8/pi))/3;%normrnd(0,0.1,[T,1])+
-plot(t,x1,'.');
+% plot(t,x1,'.');
 % hist(x1,100)
 global train_data train_label test_data test_label;
 train_data=cell(0);
 train_label=cell(0);
-lenInput=10;
+lenInput=100;
 lenOutput=10;
-num=100;
+num=10;
 index=floor(rand(num,1)*(size(x1,1)-lenInput-lenOutput));
 for i1=1:num
     range1=index(i1)+1:index(i1)+lenInput;
@@ -21,14 +21,14 @@ end
 test_data=train_data;
 test_label=train_label;
 % 参数设置
-load('args.mat');
+% load('args.mat');
 if(exist('args','var'))%梯度检查
     args.maxecho=1;
     args.circletimes=1;
     args.momentum=0;
     args.learningrate=0;
     [args]=LSTM_train(args);%计算下当前的梯度
-    vname='args.WeightEncoder{1, 2}.w_i';
+    vname='args.WeightTranP{1, 1}.w_k';
     vname=vname(6:end);
     s1=strcat('dcal=args.Mom.',vname,'(1,1);');
     eval(s1);
@@ -50,21 +50,25 @@ else %初始化
     args.momentum=0.9;
     args.learningrate=1e-1;
     args.batchsize=10;
-    dimC=20;
+    dimC=10;
     dimInput=1;
     dimOutput=1;
-    layer=[20];
+    layer=[50];
     args.encoderLayer=[1,layer,dimC];
     args.decoderLayer=[dimC+dimInput,layer,dimInput];
     args.predictLayer=[dimC+dimOutput,layer,dimOutput];
     args=LSTM_initial(args);
     [args]=LSTM_train(args);
     save('args.mat','args');
-    test_data=train_data;
-    test_label=train_label;
-    [reconstruct,predict,errorR,errorP]=LSTM_ff(test_data,test_label,args);
-    plot(1:size(test_data{1},1),test_data{1},'--o'...
-        ,size(test_data{1},1)+1:size(test_data{1},1)+size(test_label{1},1),test_label{1},'--o'...
-        ,1:size(reconstruct,1),reconstruct(end:-1:1,:),'*'...
-        ,size(reconstruct,1)+1:size(reconstruct,1)+size(predict,1),predict,'*');
+%     test_data=train_data;
+%     test_label=train_label;
+%     [reconstruct,predict,errorR,errorP]=LSTM_ff(test_data,test_label,args);
 end
+
+i1=5;
+[reconstruct,predict,errorR,errorP]=LSTM_ff(test_data(i1),{zeros(100,1)},args);
+figure;
+plot(1:size(test_data{i1},1),test_data{i1},'--o'...
+    ,size(test_data{i1},1)+1:size(test_data{i1},1)+size(test_label{i1},1),test_label{i1},'--o'...
+    ,1:size(reconstruct,1),reconstruct(end:-1:1,:),'*'...
+    ,size(reconstruct,1)+1:size(reconstruct,1)+size(predict,1),predict,'*');
