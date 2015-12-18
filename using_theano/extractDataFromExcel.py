0b9f1ab50f5f0473e1a4c7b3f8cee5430959740f
prm10 <353345737@qq.com>
2015-12-18 23:46:29 +0800

    a

--- using_theano/extractDataFromExcel.py
+++ using_theano/extractDataFromExcel.py
@@ -1,8 +1,24 @@
+# -*- coding: utf-8 -*-
 __author__ = 'prm14'
 import xlrd
+import openpyxl
+import re
 
-data = xlrd.open_workbook('data.xlsx')
+data = xlrd.open_workbook(u'二号炉产量和物理热范围.xlsx')
+table1 = data.sheet_by_name('Sheet1')
+book=openpyxl.Workbook()
+sheet=book.get_sheet_by_name(name='Sheet')
 
-table = data.sheet_by_name(u'Sheet1')
-print(map(lambda x:x.encode('gbk'),table.col_values(2)))
-
+pattern=re.compile('^(\d+)-(\d+)$')
+value=0
+for i in range(table1.nrows):
+    for j in range(table1.ncols):
+        value=table1.cell(i,j).value
+        sheet.cell(row=i+1,column=j+1,value=value)
+    match=pattern.match(value)
+    if match:
+        sheet.cell(row=i+1,column=table1.ncols+1,value=int(match.group(1)))
+        sheet.cell(row=i+1,column=table1.ncols+2,value=int(match.group(2)))
+    else:
+        print(u'第%d行第%d列不符合规范'%(i+1,table1.ncols))
+book.save('result.xlsx')
