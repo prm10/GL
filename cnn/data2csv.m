fae588b5c76a11db8b7ff51ea8c07b4118dfb99e
prm10 <353345737@qq.com>
2016-02-21 13:02:55 +0800

    a

--- cnn/data2csv.m
+++ cnn/data2csv.m
@@ -3,10 +3,11 @@ function data2csv(data_seg,len_data,len_target,csvPath,csvName)
 % low_filter(data1(range,ipt(4)))
 %% get data and target
 % sv_seg=sv(range,:);% stove change records of concerned data
-target_std=zeros(size(data_seg,1)-len_data-len_target,size(data_seg,2));
-target_mean=zeros(size(data_seg,1)-len_data-len_target,size(data_seg,2));
-for i1=1:size(data_seg,1)-len_data-len_target
-    data_target=data_seg(i1+len_data+1:i1+len_data+len_target,:);
+len_valid=size(data_seg,1)-len_data-len_target+1;
+target_std=zeros(len_valid,size(data_seg,2));
+target_mean=zeros(len_valid,size(data_seg,2));
+for i1=1:size(target_std,1)
+    data_target=data_seg(i1+len_data:i1+len_data+len_target-1,:);
     target_mean(i1,:)=median(data_target);
     target_std(i1,:)=std(data_target,0,1);
 end
